38dc31df5463cda0c7c6c01cc3361a3e1fba61c0
CuongHD98 <you@example.com>
2023-04-04 14:44:44 +0700

    Add area2 file

---
+++ Area2Js.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeigh;
+inputWidth = prompt("Enter Width");
+inputHeigh = prompt("Enter Heigh");
+ let width = parseInt(inputWidth);
+ let heigh = parseInt(inputHeigh);
+ let area = width * heigh;
+ document.write("The area is: " + area);
